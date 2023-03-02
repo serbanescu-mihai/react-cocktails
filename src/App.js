b35dc2c3b8e15db4bca87b28fcde8879c3c8f16a
Mihai Serbanescu <102154186+serbanescu-mihai@users.noreply.github.com>
2023-03-02 11:50:00 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Cocktails</h1>
+
+      <section className="container">
+        {/* single cocltail */}
+        <article className="cocktail">
+          <img
+            src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Singapore-sling-7ddad3e.jpg?quality=90&webp=true&resize=600,545"
+            alt=""
+            className="cocktail-img"
+          />
+          <h2 className="cocktail-title">Title</h2>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            illum aut nihil ullam commodi maiores alias vel doloribus ab
+            perspiciatis.
+          </p>
+          <p className="ingredients">Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        {/* single cocltail */}
+        <article className="cocktail">
+          <img
+            src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Singapore-sling-7ddad3e.jpg?quality=90&webp=true&resize=600,545"
+            alt=""
+            className="cocktail-img"
+          />
+          <h2 className="cocktail-title">Title</h2>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            illum aut nihil ullam commodi maiores alias vel doloribus ab
+            perspiciatis.
+          </p>
+          <p className="ingredients">Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        {/* single cocltail */}
+        <article className="cocktail">
+          <img
+            src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Singapore-sling-7ddad3e.jpg?quality=90&webp=true&resize=600,545"
+            alt=""
+            className="cocktail-img"
+          />
+          <h2 className="cocktail-title">Title</h2>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            illum aut nihil ullam commodi maiores alias vel doloribus ab
+            perspiciatis. Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Magni, odio quasi eum aspernatur eligendi aperiam minus
+            necessitatibus laudantium quam officia.
+          </p>
+          <p className="ingredients">Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        {/* single cocltail */}
+        <article className="cocktail">
+          <img
+            src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Singapore-sling-7ddad3e.jpg?quality=90&webp=true&resize=600,545"
+            alt=""
+            className="cocktail-img"
+          />
+          <h2 className="cocktail-title">Title</h2>
+          <p className="description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            illum aut nihil ullam commodi maiores alias vel doloribus ab
+            perspiciatis.
+          </p>
+          <p className="ingredients">Lorem ipsum dolor sit amet.</p>
+        </article>
+      </section>
+    </main>
   );
 }
 
